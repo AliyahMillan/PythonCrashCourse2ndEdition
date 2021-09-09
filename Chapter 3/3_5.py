93af1e5ab673fc8035ed2bbaeed34d7b60015fa2
Aliyah Millán <millan_1802@csu.fullerton.edu>
2021-09-09 13:24:00 -0700

    3_5.py
    Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
    -- Start with your program from 3-4. Add a print() call at the end of your program stating the name of the guest who can’t make it.
    -- Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
    -- Print a second set of invitation messages, one for each person who is still in your list.

---
+++ Chapter 3/3_5.py
@@ -0,0 +1,23 @@
+"""
+Aliyah Alexis Millán
+CPSC-223P Section 1/Section 2
+1 September 2021
+"""
+dinnerInvites = ['Alec Benjamin', 'Rian', 'Brendon Urie']
+message = "You're invited to my exclusive dinner party,"
+
+print("--->",message, dinnerInvites[0],"!")
+print("--->",message, dinnerInvites[1],"!")
+print("--->",message, dinnerInvites[2],"!")
+
+print("\n--->\nRian to Aliyah:\n I'm so sorry, Aliyah, but I won't be able to make it to your dinner party. I don't own a tux and I can't imagine showing my face around your two idols without a tux. You'll have a great time without me anyways. Maybe next time we can just go to the library and try that new htb machine? Love you, sorry, have fun! :)")
+
+print("\n<--->\nAliyah to Everyone:\n Hi everyone!", dinnerInvites[1],"chickened out and won't be able to make it to the party. :( I know you guys were all hoping to meet him, but... I expect you'll find our newest guest to be quite scintillating.\n")
+
+dinnerInvites = ['Alec Benjamin', 'Brendon Urie', 'Stephen Colbert']
+
+message = "You're invited to my exclusive dinner party,"
+
+print("--->",message, dinnerInvites[0],"!")
+print("--->",message, dinnerInvites[1],"!")
+print("--->",message, dinnerInvites[2],"!")
