f74a6d91e8d2e709dc28b6a72ed090b1cc2119e6
Aliyah Millán <millan_1802@csu.fullerton.edu>
2021-09-09 13:10:48 -0700

    2_4.py
    Name Cases: Use a variable to represent a person’s name, and then print that person’s name in lowercase, uppercase, and title case.

---
+++ Chapter2/2_4.py
@@ -0,0 +1,14 @@
+"""
+Aliyah Alexis Millán
+CPSC-223P Section 1/Section 2
+24 August 2021
+"""
+
+name = "Aliyah Millan!"
+print(name.lower())
+
+name = "Aliyah Millan!"
+print(name.upper())
+
+name = "Aliyah Millan!"
+print(name.title())
