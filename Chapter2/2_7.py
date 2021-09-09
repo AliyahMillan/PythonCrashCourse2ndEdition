5558de3f8e44a658a93854742cb38fc1be1098bc
Aliyah Millán <millan_1802@csu.fullerton.edu>
2021-09-09 13:14:17 -0700

    2_7.py
    Stripping Names: 
    Use a variable to represent a person’s name, and include some whitespace characters at the beginning and end of the name. 
    Make sure you use each character combination, "\t" and "\n", at least once. 
    Print the name once, so the whitespace around the name is displayed. 
    Then print the name using each of the three stripping functions, lstrip(), rstrip(), and strip().

---
+++ Chapter2/2_7.py
@@ -0,0 +1,17 @@
+"""
+Aliyah Alexis Millán
+CPSC-223P Section 1/Section 2
+26 August 2021
+"""
+#I exchanged the comma with a colon, since my quote is from a song lyric. I thought it was interesting.
+famous_personbefore = "Alec Benjamin"
+filler = ":"
+message = '\n\t"You have to understand that\n\tThe one I killed is me"\n'
+print(famous_personbefore.title(), filler, message)
+#Now with the whitespaces
+famous_person = " Alec Benjamin "
+print(famous_person)
+print(famous_person.lstrip())
+print
+print(famous_person.rstrip())
+(famous_person.strip())
