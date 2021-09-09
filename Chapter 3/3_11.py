1c1d5a7376590ed21a77b207e0b7c160451b98d7
Aliyah Millán <millan_1802@csu.fullerton.edu>
2021-09-09 13:32:58 -0700

    3_11.py
    Slicing: Suppose that word = "image" and phrase = "protein synthesis" write a program that displays the following string slices, i, age, im, iae, rote, protein synth, protein image.  The first line has been completed for you as an example.  Name the program string_slicing.py. ( I just named it after chapter 3, problem 11, as the professor prefers)
    
    word[0]:  i

---
+++ Chapter 3/3_11.py
@@ -0,0 +1,15 @@
+"""
+Aliyah Alexis Millán
+CPSC-223P Section 1/Section 2
+2 September 2021
+"""
+word = "image"
+phrase = "protein synthesis"
+
+print(word[0])
+print(word[2:5])
+print(word[0:2])
+print(word[0] + word[2] + word[4])
+print(phrase[1:5])
+print(phrase[0:13])
+print(phrase[0:8] + word)
