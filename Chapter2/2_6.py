8b3f8b0ace0865829060016e4e8cc5b830ce8fc2
Aliyah Millán <millan_1802@csu.fullerton.edu>
2021-09-09 13:13:05 -0700

    2_6.py
    Famous Quote 2: Repeat Exercise 2-5, but this time, represent the famous person’s name using a variable called famous_person. Then compose your message and represent it with a new variable called message. Print your message.

---
+++ Chapter2/2_6.py
@@ -0,0 +1,14 @@
+"""
+Aliyah Alexis Millán
+CPSC-223P Section 1/Section 2
+25 August 2021
+"""
+
+#I exchanged the comma with a colon, since my quote is from a song lyric. I thought it was interesting.
+famous_person = "Alec Benjamin"
+
+filler = "once said in his song 'If I Killed Someone For You':"
+
+message = '\n"You have to understand that\nThe one I killed is me"'
+
+print(famous_person.title(), filler, message)
