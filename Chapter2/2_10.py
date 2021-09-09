951d3473654b74c6e532c36197d6588364e92763
Aliyah Millán <millan_1802@csu.fullerton.edu>
2021-09-09 13:18:13 -0700

    2_10.py
    Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.

---
+++ Chapter2/2_10.py
@@ -0,0 +1,8 @@
+"""
+Aliyah Alexis Millán
+CPSC-223P Section 1/Section 2
+26 August 2021
+"""
+#I've added comments using triple quotes and this '#' symbol.
+name = "My name is Aliyah!"
+print(name)
