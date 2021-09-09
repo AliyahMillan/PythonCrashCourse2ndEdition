3208c9158e54b0bad6d10ced1687b1a1f8460e97
Aliyah Millán <millan_1802@csu.fullerton.edu>
2021-09-09 13:19:49 -0700

    2_12.py
    Starting with the following program,
    --------------------------------------------
    from math import pi
    r = 12
    area = pi* r ** 2
    print ("The area of a circle with radius",r,"is",area)
    --------------------------------------------
    modify this program to display the circumference of the circle.  
    Determine how to use the round function to limit the number of decimals displayed to two.

---
+++ Chapter2/2_12.py
@@ -0,0 +1,12 @@
+"""
+Aliyah Alexis Millán
+CPSC-223P Section 1/Section 2
+26 August 2021
+"""
+from math import pi
+r = 12
+area = pi* r ** 2
+print ("The area of a circle with radius",r,"is",area)
+
+circ = round(2*pi*r)
+print("The circumference of a circle with radius",r,"is", circ)
