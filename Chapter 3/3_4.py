0c7fe9ca3ef08fba0a20f86b7f6f12fd94eef010
Aliyah Millán <millan_1802@csu.fullerton.edu>
2021-09-09 13:23:21 -0700

    3_4.py
    Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.

---
+++ Chapter 3/3_4.py
@@ -0,0 +1,12 @@
+"""
+Aliyah Alexis Millán
+CPSC-223P Section 1/Section 2
+1 September 2021
+"""
+
+dinnerInvites = ['Alec Benjamin', 'Rian', 'Brendon Urie']
+message = "You're invited to my exclusive dinner party,"
+
+print(message, dinnerInvites[0],"!")
+print(message, dinnerInvites[1],"!")
+print(message, dinnerInvites[2],"!")
