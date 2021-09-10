36eca2aeac232a9ad95c87e890ad2d6be1f4af39
Aliyah Millán <millan_1802@csu.fullerton.edu>
2021-09-10 16:40:33 -0700

    4_8.py

---
+++ Chapter 4/4_8.py
@@ -0,0 +1,13 @@
+"""
+Aliyah Alexis Millán
+CPSC-223P Section 1/Section 2
+7 September 2021
+"""
+
+cube_list = []
+for num in range(1, 11):
+  cube = num**3
+  cube_list.append(cube)
+
+for cube in cube_list:
+  print(cube)
