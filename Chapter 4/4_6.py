a130d9b083d4f4f671281de0aca7b8f92ec70f4d
Aliyah Millán <millan_1802@csu.fullerton.edu>
2021-09-10 16:39:33 -0700

    4_6.py

---
+++ Chapter 4/4_6.py
@@ -0,0 +1,9 @@
+"""
+Aliyah Alexis Millán
+CPSC-223P Section 1/Section 2
+7 September 2021
+"""
+#Formula to follow: range([start], end, [step])
+#where step is how many number jumps you are making
+for i in range(1, 20, 2):
+  print(i)
