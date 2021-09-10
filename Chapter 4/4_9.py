3129b85811c75cf7948cc12c1e2d77fca1c7af29
Aliyah Millán <millan_1802@csu.fullerton.edu>
2021-09-10 16:42:34 -0700

    4_9.py
    Cube Comprehension: Use a list comprehension to generate a list of the first 10 cubes.

---
+++ Chapter 4/4_9.py
@@ -0,0 +1,8 @@
+"""
+Aliyah Alexis Millán
+CPSC-223P Section 1/Section 2
+8 September 2021
+"""
+cube_list = [num ** 3 for num in range(1,11)] #Allows me to create a single-lined for loop for my list as I make the list itself.
+for cube in cube_list:
+  print(cube)
