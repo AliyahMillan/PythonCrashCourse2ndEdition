09f7a01d86fbb3caec29c33463c8a8a52378e98e
Aliyah Millán <millan_1802@csu.fullerton.edu>
2021-09-10 16:45:07 -0700

    4_13.py
    Buffet: A buffet-style restaurant offers only five basic foods. Think of five simple foods, and store them in a tuple.
    
    Use a for loop to print each food the restaurant offers.
    Try to modify one of the items, and make sure that Python rejects the change.
    The restaurant changes its menu, replacing two of the items with different foods. Add a line that rewrites the tuple, and then use a for loop to print each of the items on the revised menu.

---
+++ Chapter 4/4_13.py
@@ -0,0 +1,23 @@
+"""
+Aliyah Alexis Millán
+CPSC-223P Section 1/Section 2
+8 September 2021
+"""
+menuItems = (
+  'Mac n cheese', 'Chicken nuggets', 'Cheeseburgers',
+  'Lasagne', 'Fish n chips',
+)
+
+print("Here's your menu!:")
+for item in menuItems:
+ print("- ", item)
+
+menuItems = (
+  'Mac n cheese', 'Chicken nuggets', 'Cheeseburgers',
+  'Spaghetti', 'Haggis', 
+)
+
+print("\nWe updated our menu!")
+print("Here's the new menu:")
+for item in menuItems:
+  print("- ", item)
