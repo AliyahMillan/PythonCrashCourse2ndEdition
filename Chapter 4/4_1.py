96fd8984c4a1cbb8c498f0377cf094c38b785874
Aliyah Millán <millan_1802@csu.fullerton.edu>
2021-09-10 16:37:14 -0700

    4_1.py
    Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a list, and then use a for loop to print the name of each pizza.
    - Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
    - Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!

---
+++ Chapter 4/4_1.py
@@ -0,0 +1,16 @@
+"""
+Aliyah Alexis Millán
+CPSC-223P Section 1/Section 2
+7 September 2021
+"""
+
+pizza_list = ['Cheese', 'Pepperoni', 'BBQ Chicken']
+"""
+print(' '.join(map(str, pizza_list)))
+print("In a downward list:")
+print('\n'.join(map(str, pizza_list))) #Accomplishes the same thing as a for loop
+"""
+#For loop:
+for i in range(len(pizza_list)):
+  print ("I like", pizza_list[i],".")
+print("I'm actually not that crazy about pizza, I'm sorry.\nIt's ok, but I've never really craved it.\nMy favourite kind would be cheese though. Golden, not brown.")
