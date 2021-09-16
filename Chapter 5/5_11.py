0991530252d452c8f497d975b3c4a22e306e086e
Aliyah Millán <millan_1802@csu.fullerton.edu>
2021-09-16 15:54:33 -0700

    5_11.py
    Ordinal Numbers: Ordinal numbers indicate their position in a list, such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
    
    Store the numbers 1 through 9 in a list.
    Loop through the list.
    Use an if-elif-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line.

---
+++ Chapter 5/5_11.py
@@ -0,0 +1,16 @@
+"""
+Aliyah Alexis Millán
+CPSC-223P Section 1/Section 2
+9 September 2021
+"""
+
+numbers = list(range(1,10))
+for num in numbers:
+  if num == 1:
+    print("1st")
+  elif num == 2:
+    print("2nd")
+  elif num == 3:
+    print("3rd")
+  else:
+    print(str(num) + "th")
