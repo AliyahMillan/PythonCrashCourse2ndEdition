a2e5d94c0ca3b70109738c2b58e40b19f0b8a14d
Aliyah Millán <millan_1802@csu.fullerton.edu>
2021-09-25 12:59:32 -0700

    7_1.py
    Rental Car: Write a program that asks the user what kind of rental car they would like. Print a message about that car, such as “Let me see if I can find you a Subaru.”

---
+++ Chapter 7/7_1.py
@@ -0,0 +1,8 @@
+"""
+Aliyah Alexis Millán
+CPSC-223P Section 1/Section 2
+22 September 2021
+"""
+
+carType = input("What kind of car would you like?\n")
+print("Let me see if I can find you a " + carType.title() + "!")
