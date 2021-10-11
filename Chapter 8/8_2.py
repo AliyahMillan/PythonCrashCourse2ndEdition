88481efc02438688f47fb764ac2dcc56e63cc0cb
Aliyah Millán <millan_1802@csu.fullerton.edu>
2021-10-11 13:00:40 -0700

    8_2.py
    Favorite Book: Write a function called favorite_book() that accepts one parameter, title. The function should print a message, such as One of my favorite books is Alice in Wonderland. Call the function, making sure to include a book title as an argument in the function call.

---
+++ Chapter 8/8_2.py
@@ -0,0 +1,10 @@
+"""
+Aliyah Alexis Millán
+CPSC-223P Section 1/Section 2
+5 October 2021
+"""
+
+def favorite_book(book_title):
+  print(book_title + " is one of my favorite books.")
+
+favorite_book('The Sound and the Fury')
