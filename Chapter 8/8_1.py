18fee644f7c892ca4342285dd422df4c4dc876ee
Aliyah Millán <millan_1802@csu.fullerton.edu>
2021-10-11 12:59:47 -0700

    8_1.py
     Message: Write a function called display_message() that prints one sentence telling everyone what you are learning about in this chapter. Call the function, and make sure the message displays correctly.

---
+++ Chapter 8/8_1.py
@@ -0,0 +1,11 @@
+"""
+Aliyah Alexis Millán
+CPSC-223P Section 1/Section 2
+5 October 2021
+"""
+
+def display_message():
+  message = "I'm learning to code using functions, I think."
+  print(message)
+
+display_message()
